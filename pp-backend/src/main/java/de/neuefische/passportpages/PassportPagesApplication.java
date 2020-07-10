c5102f93ea0e2a205fc2c59ba36c8cf76d413a4d
Alpay <alpay.ergindemir@live.de>
2020-07-10 17:04:27 +0200

    Setup project

---
+++ pp-backend/src/main/java/de/neuefische/passportpages/PassportPagesApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.passportpages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportPagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassportPagesApplication.class, args);
+    }
+
+}
---
+++ pp-backend/src/test/java/de/neuefische/passportpages/PassportPagesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.passportpages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassportPagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
