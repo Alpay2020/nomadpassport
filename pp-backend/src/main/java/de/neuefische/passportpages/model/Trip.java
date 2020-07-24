78802643b3bd15295bb4b6cdb92fb3dff8b929b8
Alpay <alpay.ergindemir@live.de>
2020-07-24 14:18:37 +0200

    Add application.yml to gitignore

--- pp-backend/src/main/java/de/neuefische/passportpages/model/Trip.java
+++ pp-backend/src/main/java/de/neuefische/passportpages/model/Trip.java
@@ -1,4 +1,17 @@
 package de.neuefische.passportpages.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Trip {
+
+    private Date dateTripStart;
+    private Date dateTripEnd;
+
 }
--- pp-backend/src/main/java/de/neuefische/passportpages/model/oauth/FacebookUserData.java
+++ pp-backend/src/main/java/de/neuefische/passportpages/model/oauth/FacebookUserData.java
@@ -9,6 +9,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class FacebookUserData {
     private String id;
-//    private String avatar_url;
     private String name;
 }
