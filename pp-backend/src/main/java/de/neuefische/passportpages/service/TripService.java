5c641a15288b7ed0db69b8c1ab0a02c6682eca2e
Alpay <alpay.ergindemir@live.de>
2020-08-07 17:13:52 +0200

    Add get future trips method and get past trips method

--- pp-backend/src/main/java/de/neuefische/passportpages/service/TripService.java
+++ pp-backend/src/main/java/de/neuefische/passportpages/service/TripService.java
@@ -6,6 +6,13 @@ import de.neuefische.passportpages.utils.IdUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 @Service
 public class TripService {
 
@@ -17,6 +24,7 @@ public class TripService {
         this.idUtils = idUtils;
         this.tripMongoDb = tripMongoDb;
     }
+
     public Trip add(String dateTripStart, String dateTripEnd, String destinationCountry) {
         Trip trip = new Trip();
         trip.setId(idUtils.generateRandomId());
@@ -33,4 +41,55 @@ public class TripService {
     public void deleteTrip(String id) {
         tripMongoDb.deleteById(id);
     }
+
+    public Iterable<Trip> getFutureTrips() {
+        //final list
+        List<Trip> futureTrips = new ArrayList<>();
+
+        //current date
+        LocalDate currentDate = LocalDate.now();
+
+        //database listfetch
+        List<Trip> tripList = (List<Trip>) tripMongoDb.findAll();
+
+        //loop through triplist
+        for (int i = 0; i < tripList.size(); i++) {
+            //get single trip-date
+            String tripDate = tripList.get(i).getDateTripEnd();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+            LocalDate dateTime = LocalDate.parse(tripDate, formatter);
+            //compare trip-date and current date
+            if (currentDate.compareTo(dateTime) <= 0) {
+                // if tripdate
+                futureTrips.add(tripList.get(i));
+            }
+        }
+        return futureTrips;
+    }
+
+    public Iterable<Trip> getPastTrips() {
+        //final list
+        List<Trip> pastTrips = new ArrayList<>();
+
+        //current date
+        LocalDate currentDate = LocalDate.now();
+
+        //database listfetch
+        List<Trip> tripList = (List<Trip>) tripMongoDb.findAll();
+
+        //loop through triplist
+        for (int i = 0; i < tripList.size(); i++) {
+            //get single trip-date
+            String tripDate = tripList.get(i).getDateTripEnd();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+            LocalDate dateTime = LocalDate.parse(tripDate, formatter);
+            //compare trip-date and current date
+            if (currentDate.compareTo(dateTime) >= 0) {
+                // if tripdate
+                pastTrips.add(tripList.get(i));
+            }
+        }
+        return pastTrips;
+    }
+
 }
